9d87bd6b08395ccd14c2666d57689dd5028e393a
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2022-03-09 10:24:22 +0000

    test in large numbers

---
+++ compare_sort.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -cp ./bin ram.Ram ./src/test/insertion.ram ./input_tape.in ./output_tape.out 1
+java -cp ./bin ram.Ram ./src/test/bubbleSort.ram ./input_tape.in ./output_tape.out 1
