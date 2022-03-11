afc1a0acb913884ab8c6c0aad96b6398ea41e3d9
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2022-03-11 09:39:10 +0000

    fixed2

--- compare_sort.sh
+++ compare_sort.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo
 echo "InsertionSort"
 java -cp ./bin ram.Ram ./src/test/insertion.ram ./input_tape.in ./output_tape.out 1
+echo
 echo "BubbleSort"
 java -cp ./bin ram.Ram ./src/test/bubbleSort.ram ./input_tape.in ./output_tape.out 1
